3f191565800f8eec4e1c34c48ba93cc93a12a96b
Tanuja <tanujashan30@gmail.com>
2022-04-29 15:00:22 +0530

    Commited the file

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   var interest = principal * years * rate /100;
+   var years = new Date().getFullYear()+parseInt(years);
+   document.getElementById("result").innerHTML ="<br>If you deposit <mark>"+ principal+"</mark>,<br>"+"at an interest rate of <mark>"+rate+"</mark><br>"+"You will receive an amount of <mark>"+ interest+"</mark><br>"+"in the year <mark>"+ years+"</mark>";
+
+
+   if (principal <= 0) {
+    alert("Enter a positive number");
+
+    document.getElementById("principal").focus();
+    document.getElementById("principal").click();
 }
-        
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
+}
